2f15d2963c86392f0fdb3037b3061dedbb34f255
eudoracheng <eudoracheng@foxmail.com>
2018-01-31 15:53:21 +0800

    create albums

--- controller/router.js
+++ controller/router.js
@@ -5,9 +5,25 @@
 
 // exports.showIndex = showIndex;
 
+// 读取models里用于返回文件名数组的模块
+var file = require("../models/files.js")
+
 // 首页
 exports.showIndex = (req,res)=>{
-    res.send("欢迎来到首页");
+    //调用models里的files文件的方法。读取uploads里的文件夹名称数组呈递给ejs做页面渲染
+    // albums:files.getAllAlbum() 
+    file.getAllAlbums(function(err,allAlbums){
+        if(err){
+            res.send(err);
+            return;
+        }
+        res.render("index",{
+            "albums":allAlbums
+        })
+    })
+        
 }
 // 相册页
-exports.showAlbum = (req,res)=>res.send("相册"+ req.params.albumName);
+exports.showAlbum = (req,res)=>{
+    res.send("相册"+ req.params.albumName);
+}
---
+++ models/files.js
@@ -0,0 +1,38 @@
+// 定义 一个返回文件名称数组的功能模块 并暴露出去等待调用
+
+var fs = require("fs");
+
+// exports.getAllAlbum = (req,res)=>{
+//     return ["小明","小工"]
+// }
+
+exports.getAllAlbums = function(callback){
+    fs.readdir("./uploads",(err,files)=>{
+        // files为uploads里的内容的数组
+        if(err){
+            callback("没有找到uploads文件夹",null)
+        }
+
+
+        var allAlbums = [];
+        // nodejs异步不会顺序遍历uploads里的文件夹因此需要一个迭代器强制遍历
+        (function iterator(i){
+            if(i == files.length){
+                // 理论上如果迭代到最后一个就返回数组，但是nodejs是异步的不会等到return结果输出 ,而是要把结果当成一个参数回调给父级的函数
+                // return allAlbums
+                callback(null,allAlbums);
+                return;
+            }
+            fs.stat("./uploads/"+files[i],(err,stats)=>{
+                if(err){
+                    callback("找不到文件"+files[i],null)
+                }
+                if(stats.isDirectory()){
+                    allAlbums.push(files[i])
+                }
+                iterator(i+1);
+            })
+        }(0));
+
+    })
+}
